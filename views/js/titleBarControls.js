4eee6f43865bcb29ba96eb9f09e455e5b03d4d10
sravan <sravannerella007@hotmail.com>
2017-08-16 17:41:56 -0400

    Initial Commit 0.02 (Alpha)
    + Tab System
    + Double click on tab bar for new tab
    + Text Editor

--- views/js/titleBarControls.js
+++ views/js/titleBarControls.js
@@ -1,5 +1,5 @@
 const $ = require('jquery');
-window.jQuery = $;
+window.$ = window.jQuery = $;
 const {remote} = require('electron');
 let win = remote.getCurrentWindow();
 
@@ -19,4 +19,27 @@ $(".maximize").click(function(){
 
 $(".close").click(function(){
 	win.close();
+});
+
+
+$("#itemBar").on('click', '.tabClose', function(){
+	var anchor = $(this).parent();
+    $(anchor.attr('href')).remove();
+	$(this).parent().parent().remove();
+	$(".nav-tabs li").children('a').first().click();
+});
+
+
+$("#itemBar").on('click', '#item', function(e){
+	e.preventDefault();
+	console.log($(this).children('a').attr('href'));
+	$($(this)).tab('show');
+});
+
+$("#itemBar").on('dblclick', function(){
+	var id = $("#itemBar").children().length;
+	$('#itemBar').append('<li role="presentation" id="item" class="nav-item"><a href="#new'+ id +'" aria-controls="new'+ id +'" data-toggle="tab" role="tab" class="nav-link">untitled<span class="tabClose"><i class="material-icons s18">close</i></span></a></li>');
+	$('.tab-content').append('<div role="tabpanel" class="tab-pane" aria-expanded="false" id="new'+id+'"><p>Text'+id+'</p></div>');
+	var nid = '#new'+id;
+	$(nid).click();
 });
